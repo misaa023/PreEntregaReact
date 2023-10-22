a67a78ace1781513c0aec4be695e82f36adf676e
Esteban Madrid <misaa023@gmail.com>
2023-10-22 18:54:41 -0300

    primer PreEntrega Madrid Esteban

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { CardWidget } from './CardWidget';
 export const NavBar = () => {
     return (
         <nav className=" bg-slate-800 text-white p-3">
-            <h1 className="ml-20">LOGO</h1>
+            <h1 className="ml-24">LOGO</h1>
             <ul className="flex space-x-16 justify-end mr-9">
                 <li>
                     <a href="#">Inicio</a>
