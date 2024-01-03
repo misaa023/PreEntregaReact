a39cb26b016a5a0bdc75b8f7e5bcda88f580310e
Esteban Madrid <misaa023@gmail.com>
2024-01-03 15:03:34 -0300

    se agrego firestore a itemDDetailContainer para poder ver el precio de los items

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,24 +1,22 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+// firestore
+import { getFirestore, doc, getDoc, query, where } from 'firebase/firestore';
 import { ItemDetail } from './ItemDetail';
-
-// import { products } from '../data/products';
 
 export const ItemDetailContainer = () => {
     const [item, setItem] = useState(null);
     const { id } = useParams();
 
-    // useEffect(() => {
-    //     const mypromise = new Promise((resolve, reject) => {
-    //         setTimeout(() => {
-    //             resolve(products);
-    //         }, 2000);
-    //     });
-    //     mypromise.then((response) => {
-    //         const findById = response.find((item) => item.id === Number(id));
-    //         setItem(findById);
-    //     });
-    // }, [id]);
+    useEffect(() => {
+        const db = getFirestore();
+
+        const refDoc = doc(db, 'items', id);
+
+        getDoc(refDoc).then((snapshot) => {
+            setItem({ id: snapshot.id, ...snapshot.data() });
+        });
+    }, [id]);
 
     return <div>{item ? <ItemDetail item={item} /> : <>Loading...</>}</div>;
 };
