de3922044c9818d75ce17c9fffee19cdf2e73d27
Esteban Madrid <misaa023@gmail.com>
2024-01-03 17:36:18 -0300

    logo mas estilos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import { CardWidget } from './CardWidget';
+import logo from '../assets/logo2.jpg';
 export const NavBar = () => {
     return (
-        <nav className=" bg-slate-800 text-white p-3">
-            <h1 className="ml-24">LOGO</h1>
-            <ul className="flex space-x-16 justify-end mr-9">
+        <nav className=" text-white p-1 nav-bar">
+            <img src={logo} alt="logo de la pagina" className="logo-img" />
+            <ul className="flex space-x-16  justify-end mr-20 menu-link">
                 <NavLink to="/">Inicio</NavLink>
                 <NavLink to="category/cerveza">Cerveza</NavLink>
-                <NavLink to="category/gaseosa">gaseosa</NavLink>
+                <NavLink to="category/gaseosa">Gaseosa</NavLink>
 
                 <li>
                     <CardWidget />
