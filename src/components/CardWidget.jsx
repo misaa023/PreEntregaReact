a36962663be2bb8825248598fc659117c7027da8
Esteban Madrid <misaa023@gmail.com>
2024-01-05 20:27:06 -0300

    se agrego el contador al carrito

--- src/components/CardWidget.jsx
+++ src/components/CardWidget.jsx
@@ -1,8 +1,17 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext';
 export const CardWidget = () => {
+    const { items } = useContext(CartContext);
+
+    const total = items.reduce(
+        (acumulador, valorActual) => acumulador + valorActual.quantity,
+        0,
+    );
+
     return (
         <Link to="/cart">
-            <span className="text-xl ml-10">🛒</span>
+            <span className="text-xl ml-10">🛒 {total}</span>
         </Link>
     );
 };
